ff90fcf0a8e877cfee79b9f1c420982cd0a5cf47
Santiago-Rivero <santiagonrivero@gmail.com>
2022-07-19 18:05:44 -0300

    added Navbar component

---
+++ src/components/layout/header/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        
+        <ul style={{display:"flex", justifyContent: "space-around"}}>
+            <li>Home</li>
+            <li>Proyectos</li>
+            <li>Nosotros</li>
+            <li>Contactanos</li>
+        </ul>
+        
+    )
+}
+
+export default Navbar
