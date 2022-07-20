50a3bc4430122f475f239f19183b490e18180a66
Danilo Prieto <danilo.s.prieto@gmail.com>
2022-07-19 21:43:17 -0300

    added navbar

---
+++ src/components/organisms/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        
+        <Header/>
+        
+    )
+}
+
+export const Navbar = () => {
+    return (
+        
+        <ul style={{display:"flex", justifyContent: "space-around"}}>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>About us</li>
+            <li>Contact us</li>
+        </ul>
+        
+    )
+}
+
+export default Header
